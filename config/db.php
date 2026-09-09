109c83a5d2457b98ec24335a82b56ca05fa003b9
abuu94 <abuu.yus@gmail.com>
2026-09-09 17:28:04 +0300

    From Mysql to Postgre so as to use Free tier

--- config/db.php
+++ config/db.php
@@ -1,31 +1,15 @@
+
 <?php
-// Database connection (MySQLi Procedural)
-$host = "127.0.0.1";
-$user = "root";
-$pass = "";
-$db   = "openclass";
-
-$conn = mysqli_connect($host, $user, $pass, $db);
-
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
+try {
+    $dsn = "pgsql:host=" . getenv("DB_HOST") . ";port=" . getenv("DB_PORT") . ";dbname=" . getenv("DB_NAME");
+    $conn = new PDO($dsn, getenv("DB_USER"), getenv("DB_PASS"), [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+} catch (PDOException $e) {
+    die("Connection failed: " . $e->getMessage());
 }
 ?>
 
 
-<!-- Assalaam alaykum.      Walio tia nia ya Mchango/Mkopo wa Adil:  :
-
-1. Ustadh (Mlezi) Ali Aboud    :       ✅ (Received) 100,000
-2. Ustadh (Mlezi) Talib Juma :        ✅  (Received)  30,000
-
-3. Ustadh (Mlezi) Omar Abubakar : ✅ (Received)       30,000
-4. Ustadh  Abdulhamid    :                ✅ (Received) 28,000
-
-5. Ustadh Ahmad Mdunga :            ✅ (Received)   30,000
-6. Ustadh Salum Mbarouk :             ✅(Received)   50,000
-Namba ya Kuwasilisha muamala ni   0625882085 Adil osman
-Tunapendelea In shaa Allah Mwisho wa kuwasilisha huu mchango/mkopo iwe Jumanne : Tarehe 8 September 2026. Shukran
 
 
-
-268000 -->
